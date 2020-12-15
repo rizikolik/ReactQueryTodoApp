26d938987b4c9df5d127268a37b772a5b5c5df00
rizikolik <ynsture@gmail.com>
2020-12-15 23:03:33 +0100

    refactored code ,updated add methode,checkbox and input areas added

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,28 +1,56 @@
+import React, {useState, useRef} from 'react';
 import {addDocumentsToCollection, getTodos} from '../FireBase/index';
-import {useQuery, QueryClient} from 'react-query';
+import {useQuery, QueryClient, useMutation} from 'react-query';
 function Todo() {
+  const [name, setName] = useState('');
+  const [completed, setCompleted] = useState(false);
   //Get The Client
   const queryClient = new QueryClient();
-
   const {data, error, isLoading, isError} = useQuery('todos', getTodos);
-  const handleAdd = data => {
-    addDocumentsToCollection(data);
-  };
+
+  const mutateAdd = useMutation('todos', addDocumentsToCollection, {
+    onSuccess: () => {
+      // Invalidate and refetch =>Forcing api to refresh (This is one of the points where this is differant from normal api calls..)
+      queryClient.invalidateQueries('todos');
+    },
+  });
+
   if (isLoading) {
     return <p>LOADİNG</p>;
   }
   if (isError) {
     return <p>{error}</p>;
+  } else {
+    return (
+      <>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <input
+            type='text'
+            name='name'
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <button onClick={() => mutateAdd.mutate({name})}>Submit</button>
+        <React.Fragment>
+          {data.map(data => {
+            return (
+              <div
+                style={{
+                  textAlign: 'left',
+                  display: 'flex',
+                  flexDirection: 'row',
+                }}
+              >
+                <p>{data.name} </p>
+                <input type='checkbox' defaultValue={data.completed} />
+              </div>
+            );
+          })}
+          <hr />
+          <div>React app with react query</div>
+        </React.Fragment>
+      </>
+    );
   }
-  return (
-    <div className='App'>
-      {console.log(data)}
-      {data.map(data => {
-        return <ul>{data.name}</ul>;
-      })}
-      <hr />
-      <div>React app with react query</div>
-    </div>
-  );
 }
 export default Todo;
